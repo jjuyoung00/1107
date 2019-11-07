f067385a63c1a3bf825a154438be54339f899e75
jjuyoung00 <55523928+jjuyoung00@users.noreply.github.com>
2019-11-07 15:16:48 +0900

    Add files via upload

---
+++ exam01.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+	print_stars();
+	printf("Hello C world\n");
+	print_stars();
+	
+	
+	return 0;
+}
+
+void print_stars()
+{
+	printf("**************************\n");
+}
